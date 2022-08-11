793bb708fc3dc625c9ad38e734746bb8c2147e62
JasonDesktop <MaJason93@gmail.com>
2022-08-11 07:52:15 -0700

    Testing

--- server/server.js
+++ server/server.js
@@ -4,56 +4,60 @@ const path = require('path');
 require('dotenv').config();
 
 const { typeDefs, resolvers } = require('./schemas');
-// const { authMiddleware } = require('./utils/auth');
+
 
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 4000;
-const app = express();
-// Congigring dotenv to use my key for the NY times search form
 
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  playground: true,
-  // playground: {
-	// 	endpoint: `https://exquisite-unlimited-website.herokuapp.com/graphql`,
-	// 	settings: {
-  //     		'editor.theme': 'dark'
-  //   	},
-  //   },
-  introspection: true,
-  // context: authMiddleware,
-});
 
-server.applyMiddleware({ app });
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    playground: true,
+    introspection: true,
+  });
+  await server.start();
+  const app = express();
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+  server.applyMiddleware({ app });
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    if (req) {
+      console.log("A request has been sent");
+    }
+    if (res) {
+      console.log("A response has been generated");
+      console.log(res)
+    }
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+  
+  
+  // await new Promise(resolve => app.listen({ port: 4000 }, resolve));
+  // console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  return { server, app };
 }
 
+startApolloServer();
 
-app.get('*', (req, res) => {
-  if (req) {
-    console.log("A request has been sent");
-  }
-  if (res) {
-    console.log("A response has been generated");
-    console.log(res)
-  }
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+
+
+
 
 
 // db.objectives.find().sort( { category: -1 } );
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
